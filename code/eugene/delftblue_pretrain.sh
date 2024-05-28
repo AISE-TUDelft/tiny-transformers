33aa8db5e0a083e002bd3e4b511bd9bf0958c4bf
Yijun Wu <Y.Wu-55@student.tudelft.nl>
2024-05-28 16:44:00 +0200

    added wrapper around pretrain.py for delftblue

--- code/eugene/delftblue_pretrain.sh
+++ code/eugene/delftblue_pretrain.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -l
 #
 #SBATCH --job-name="pretrain"
 #SBATCH --partition=gpu-a100
@@ -10,7 +10,20 @@
 #SBATCH --time=00:05:00 # debug mode
 #SBATCH --account=Education-EEMCS-Courses-CSE3000
 
+cd ~/CSE3000/tiny-transformers/code/eugene/
+
 module load 2023r1 openmpi miniconda3 cuda/11.7
 
-cd ~/CSE3000/tiny-transformers/code/eugene/
-srun bash -l pretrain_all.sh 0 true # debug mode
+conda activate pretrain
+
+# Assign arguments to variables
+DEBUG=$1
+MODEL_TYPE=$2
+SPARSITY_TYPE=$3
+SPARSITY_LEVEL=$4
+
+if [ "$DEBUG" = "true" ]; then
+    srun python pretrain.py --debug --model_type $MODEL_TYPE --gpu 0 --sparsity_type $SPARSITY_TYPE --sparsity_level $SPARSITY_LEVEL
+else
+    srun python pretrain.py --model_type $MODEL_TYPE --gpu 0 --sparsity_type $SPARSITY_TYPE --sparsity_level $SPARSITY_LEVEL
+fi
