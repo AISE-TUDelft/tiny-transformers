b770a84a04810645ed3aa5551d4610f75ca7f534
Yijun Wu <Y.Wu-55@student.tudelft.nl>
2024-05-29 11:11:25 +0200

    bug fix

--- code/eugene/delftblue_pretrain_all.sh
+++ code/eugene/delftblue_pretrain_all.sh
@@ -5,7 +5,7 @@ for model_type in 'gpt' 'roberta'; do
     for sparsity_type in 'baseline' 'moe'; do
         # Inner loop: 'low', 'medium', and 'high'
         for sparsity_level in 'low' 'medium'; do
-            sbatch delfblue_pretrain.sh true $model_type $sparsity_type $sparsity_level
+            sbatch delftblue_pretrain.sh true $model_type $sparsity_type $sparsity_level
         done
     done
 done
