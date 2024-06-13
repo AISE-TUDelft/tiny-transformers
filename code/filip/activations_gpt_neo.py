164c49fdb54c1e2815a9160b9f2366517fe7718c
Filip <filip.ignijic@gmail.com>
2024-06-13 17:33:06 +0200

    config neo fixed

--- code/filip/activations_gpt_neo.py
+++ code/filip/activations_gpt_neo.py
@@ -1,131 +1,11 @@
 import torch
 from torch import nn, Tensor
+import torch.nn.functional as F
 import math
 
 from transformers import GPTNeoForCausalLM, GPTNeoForSequenceClassification, GPTNeoConfig
 from transformers import AutoConfig, AutoModelForCausalLM, AutoModelForSequenceClassification
 from activations_config_neo import ActivationsGPTNeoConfig
-
-class GPTNeoPreTrainedModel(PreTrainedModel):
-    """
-    An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
-    models.
-    """
-
-    config_class = ActivationsGPTNeoConfig
-    load_tf_weights = load_tf_weights_in_gpt_neo
-    base_model_prefix = "transformer"
-    supports_gradient_checkpointing = True
-    _no_split_modules = ["GPTNeoBlock"]
-    _skip_keys_device_placement = "past_key_values"
-    _supports_flash_attn_2 = True
-
-    def __init__(self, *inputs, **kwargs):
-        super().__init__(*inputs, **kwargs)
-
-    def _init_weights(self, module):
-        """Initialize the weights."""
-        if isinstance(module, (nn.Linear,)):
-            # Slightly different from the TF version which uses truncated_normal for initialization
-            # cf https://github.com/pytorch/pytorch/pull/5617
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-
-GPT_NEO_START_DOCSTRING = r"""
-
-    This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
-    library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
-    etc.)
-
-    This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
-    Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
-    and behavior.
-
-    Parameters:
-        config ([`GPTNeoConfig`]): Model configuration class with all the parameters of the model.
-            Initializing with a config file does not load the weights associated with the model, only the
-            configuration. Check out the [`~PreTrainedModel.from_pretrained`] method to load the model weights.
-"""
-
-GPT_NEO_INPUTS_DOCSTRING = r"""
-    Args:
-        input_ids (`torch.LongTensor` of shape `(batch_size, input_ids_length)`):
-            `input_ids_length` = `sequence_length` if `past_key_values` is `None` else
-            `past_key_values[0][0].shape[-2]` (`sequence_length` of input past key value states). Indices of input
-            sequence tokens in the vocabulary.
-
-            If `past_key_values` is used, only `input_ids` that do not have their past calculated should be passed as
-            `input_ids`.
-
-            Indices can be obtained using [`AutoTokenizer`]. See [`PreTrainedTokenizer.encode`] and
-            [`PreTrainedTokenizer.__call__`] for details.
-
-            [What are input IDs?](../glossary#input-ids)
-        past_key_values (`Tuple[Tuple[torch.Tensor]]` of length `config.num_layers`):
-            Contains precomputed hidden-states (key and values in the attention blocks) as computed by the model (see
-            `past_key_values` output below). Can be used to speed up sequential decoding. The `input_ids` which have
-            their past given to this model should not be passed as `input_ids` as they have already been computed.
-        attention_mask (`torch.FloatTensor` of shape `(batch_size, sequence_length)`, *optional*):
-            Mask to avoid performing attention on padding token indices. Mask values selected in `[0, 1]`:
-
-            - 1 for tokens that are **not masked**,
-            - 0 for tokens that are **masked**.
-
-            [What are attention masks?](../glossary#attention-mask)
-        token_type_ids (`torch.LongTensor` of shape `(batch_size, input_ids_length)`, *optional*):
-            Segment token indices to indicate first and second portions of the inputs. Indices are selected in `[0,
-            1]`:
-
-            - 0 corresponds to a *sentence A* token,
-            - 1 corresponds to a *sentence B* token.
-
-            [What are token type IDs?](../glossary#token-type-ids)
-        position_ids (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
-            Indices of positions of each input sequence tokens in the position embeddings. Selected in the range `[0,
-            config.max_position_embeddings - 1]`.
-
-            [What are position IDs?](../glossary#position-ids)
-        head_mask (`torch.FloatTensor` of shape `(num_heads,)` or `(num_layers, num_heads)`, *optional*):
-            Mask to nullify selected heads of the self-attention modules. Mask values selected in `[0, 1]`:
-
-            - 1 indicates the head is **not masked**,
-            - 0 indicates the head is **masked**.
-
-        inputs_embeds (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`, *optional*):
-            Optionally, instead of passing `input_ids` you can choose to directly pass an embedded representation. This
-            is useful if you want more control over how to convert `input_ids` indices into associated vectors than the
-            model's internal embedding lookup matrix.
-
-            If `past_key_values` is used, optionally only the last `inputs_embeds` have to be input (see
-            `past_key_values`).
-        use_cache (`bool`, *optional*):
-            If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding (see
-            `past_key_values`).
-        output_attentions (`bool`, *optional*):
-            Whether or not to return the attentions tensors of all attention layers. See `attentions` under returned
-            tensors for more detail.
-        output_hidden_states (`bool`, *optional*):
-            Whether or not to return the hidden states of all layers. See `hidden_states` under returned tensors for
-            more detail.
-        return_dict (`bool`, *optional*):
-            Whether or not to return a [`~utils.ModelOutput`] instead of a plain tuple.
-"""
-
-
-@add_start_docstrings(
-    "The bare GPT Neo Model transformer outputting raw hidden-states without any specific head on top.",
-    GPT_NEO_START_DOCSTRING,
-)
-
 
 class LearnableGELU(nn.Module):
     def __init__(self, num_parameters: int = 1, init: float = 1.0,
@@ -161,24 +41,110 @@ class NeoLearnableGELUMLP(nn.Module):
         hidden_states = self.c_proj(hidden_states)
         hidden_states = self.dropout(hidden_states)
         return hidden_states
+
+# Fix MLP for GPT-Neo with PReLU
+class NeoPReLUMLP(nn.Module):
+    def __init__(self, intermediate_size, config):  # in MLP: intermediate_size= 4 * hidden_size
+        super().__init__()
+        embed_dim = config.hidden_size
+        self.c_fc = nn.Linear(embed_dim, intermediate_size)
+        self.c_proj = nn.Linear(intermediate_size, embed_dim)
+        self.act = nn.PReLU(num_parameters=intermediate_size // 2, init=0.25)
+        self.dropout = nn.Dropout(float(config.resid_dropout))
+
+    def forward(self, hidden_states):
+        hidden_states = self.c_fc(hidden_states)
+        hidden_states = self.act(hidden_states)
+        hidden_states = self.c_proj(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        return hidden_states
+
+#REF: http://arxiv.org/abs/1710.05941
+class LearnableSwish(nn.Module):
+    def __init__(self, num_parameters: int = 1, init: float = 0.25,
+                 device=None, dtype=None) -> None:
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        self.num_parameters = num_parameters
+        super().__init__()
+        self.init = init
+        self.beta = nn.Parameter(torch.empty(num_parameters, **factory_kwargs))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        torch.nn.init.constant_(self.beta, self.init)
+
+    def forward(self, x):
+        # Apply the learnable Swish function
+        return x * F.silu(self.beta * x)
+
+
+# Fix MLP for GPT-Neo with Swish
+class NeoSwishMLP(nn.Module):
+    def __init__(self, intermediate_size, config):  # in MLP: intermediate_size= 4 * hidden_size
+        super().__init__()
+        embed_dim = config.hidden_size
+        self.c_fc = nn.Linear(embed_dim, intermediate_size) # to match size for GeGLU
+        self.c_proj = nn.Linear(intermediate_size, embed_dim)
+        self.act = LearnableSwish(num_parameters=intermediate_size, init=1.0) # according to http://arxiv.org/abs/1710.05941
+        self.dropout = nn.Dropout(float(config.resid_dropout))
+
+    def forward(self, hidden_states):
+        hidden_states = self.c_fc(hidden_states)
+        hidden_states = self.act(hidden_states)
+        hidden_states = self.c_proj(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        return hidden_states
+
+from efficient_kan.model import KAN
+class KANtoMLP(nn.Module):
+
+    def __init__(self, hidden_size, inner_dim):
+        super().__init__()
+        self.c_fc = KAN([hidden_size, inner_dim])
+        self.c_proj = KAN([inner_dim, hidden_size])
+        self.act = nn.GELU()
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.act(x)
+        x = self.c_proj(x)
+        return x
     
 class ActivationsGPTNeoForCausalLM(GPTNeoForCausalLM):
-    def __init__(self, config):
+    config_class = ActivationsGPTNeoConfig
+    def __init__(self, config: ActivationsGPTNeoConfig):
         super().__init__(config)
-        # Override MLP with KAN in each transformer block
-        if config.activation_function == 'learnable_gelu':
+        if config.custom_activation == 'learnable_gelu':
             for block in self.transformer.h:
                 block.mlp = NeoLearnableGELUMLP(config.intermediate_size, config)
+        elif config.custom_activation == 'prelu':
+            for block in self.transformer.h:
+                block.mlp = NeoPReLUMLP(config.intermediate_size, config)
+        elif config.custom_activation == 'swish':
+            for block in self.transformer.h:
+                block.mlp = NeoSwishMLP(config.intermediate_size, config)
+        elif config.custom_activation == 'kan':
+            for block in self.transformer.h:
+                block.mlp = KANtoMLP(config.hidden_size, config.intermediate_size)
+     
 
 class ActivationsGPTNeoForSequenceClassification(GPTNeoForSequenceClassification):
-    def __init__(self, config):
+    config_class = ActivationsGPTNeoConfig
+    def __init__(self, config:  ActivationsGPTNeoConfig):
         super().__init__(config)
-        # Override MLP with KAN in each transformer block
-        if config.activation_function == 'learnable_gelu':
+        if config.custom_activation == 'learnable_gelu':
             for block in self.transformer.h:
                 block.mlp = NeoLearnableGELUMLP(config.intermediate_size, config)
+        elif config.custom_activation == 'prelu':
+            for block in self.transformer.h:
+                block.mlp = NeoPReLUMLP(config.intermediate_size, config)
+        elif config.custom_activation == 'swish':
+            for block in self.transformer.h:
+                block.mlp = NeoSwishMLP(config.intermediate_size, config)
+        elif config.custom_activation == 'kan':
+            for block in self.transformer.h:
+                block.mlp = KANtoMLP(config.hidden_size, config.intermediate_size)
 
 AutoConfig.register('activations_gpt_neo', ActivationsGPTNeoConfig)
 AutoModelForCausalLM.register(ActivationsGPTNeoConfig, ActivationsGPTNeoForCausalLM)
-print('test')
 AutoModelForSequenceClassification.register(ActivationsGPTNeoConfig, ActivationsGPTNeoForSequenceClassification)
