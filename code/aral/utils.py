3c3ea458840de44a48fcfb173298396c43813acd
Aral <me@aral.cc>
2024-06-18 19:08:07 +0200

    update for readibility & better error handling

--- code/aral/utils.py
+++ code/aral/utils.py
@@ -1,5 +1,8 @@
-import os, torch, wandb, numpy as np, random
+import os, torch, wandb, numpy as np, random, datetime as dt, time
 from datasets import load_from_disk, load_dataset
+from pprint import pprint
+from safetensors.torch import load_model
+
 from transformers import (
     RobertaTokenizerFast, GPT2TokenizerFast, 
     Trainer, TrainingArguments, DataCollatorForLanguageModeling,
@@ -18,16 +21,12 @@ def set_all_seeds(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-
 def get_dataset(context_length=512, debug=False):
 
     num_proc = 5 if debug else 64
     dataset_path = f'./tokenized_dataset/{context_length}'
 
-    if os.path.exists(dataset_path):
-        dataset = load_from_disk(dataset_path, keep_in_memory=True)
-
-    else: 
+    if not os.path.exists(dataset_path):
         tok_gpt = GPT2TokenizerFast.from_pretrained('common/10k-tok')
         tok_gpt.model_max_length = context_length
 
@@ -38,6 +37,8 @@ def get_dataset(context_length=512, debug=False):
 
         dataset.save_to_disk(dataset_path, num_proc=5)
 
+
+    dataset = load_from_disk(dataset_path, keep_in_memory=True)
     if debug: # only use 1% of the dataset 
         dataset['train'] = dataset['train'].select(range(len(dataset['train'])//100))
         dataset['validation'] = dataset['validation'].select(range(len(dataset['validation'])//100))
@@ -75,6 +76,14 @@ class Hyperparams(GridSearch):
     def model(self):
         if not hasattr(self, '_model'):
             self._model = self.model_config.create_model()
+            checkpoint_dir = os.path.join(self.output_dir, 'checkpoints')
+            if os.path.exists(checkpoint_dir):
+
+                # NOTE: we also reduce training epochs proportionally when loading the trainer
+                last_checkpoint = os.listdir(checkpoint_dir)[-1]
+                print(f'Loading from last checkpoint: {last_checkpoint}')
+                self._model = self._model.from_pretrained(os.path.join(checkpoint_dir, last_checkpoint))
+                self.last_checkpoint = last_checkpoint
         return self._model
 
     @property
@@ -100,6 +109,12 @@ class Hyperparams(GridSearch):
     @property
     def trainer(self) -> Trainer: 
 
+        model = self.model
+        tokenizer = self.tokenizer
+
+        dataset = self.get_dataset()
+        train_ds, eval_ds = dataset['train'], dataset['validation']
+
         training_args = TrainingArguments(
 
             seed       = 42,
@@ -118,16 +133,9 @@ class Hyperparams(GridSearch):
             save_steps          = self.eval_steps,
 
             logging_first_step  = True,
-            logging_steps       = self.eval_steps,
+            logging_steps       = self.eval_steps / 100,
             report_to           = 'wandb' if not self.debug else 'none',
         )
-
-        model = self.model
-        tokenizer = self.tokenizer
-
-        dataset = self.get_dataset()
-        train_ds, eval_ds = dataset['train'], dataset['validation']
-
         trainer = Trainer(
 
             model               = model, 
@@ -141,54 +149,102 @@ class Hyperparams(GridSearch):
 
         # print amount of training steps, and how often the model is evaluated
         print(f'''
-        Retrieving Trainer for \033[1m{self.model_name}\033[0m ({model.num_parameters():,}M)
+    Retrieving Trainer for \033[1m{self.model_name}\033[0m ({model.num_parameters():,}M)
+    Saving to {training_args.output_dir}. training on GPU: {torch.cuda.is_available()}
 
-            Training for {self.num_train_epochs} epochs, {len(train_ds)} samples
-            {self.batch_size} batch size, {self.gradient_accumulation_steps} accumulation steps.
-            Evaluating every {self.eval_steps} steps, {len(eval_ds)} samples.
-        
-        Saving to {training_args.output_dir}
+        Training for {self.num_train_epochs} epochs, {len(train_ds)} samples
+        {self.batch_size} batch size, {self.gradient_accumulation_steps} accumulation steps.
+        Evaluating every {self.eval_steps} steps, {len(eval_ds)} samples.
+    
         ''')
 
+        self.trainer = trainer 
         return trainer
 
-    def train_and_eval(self):
+    def should_continue_wandb(self) -> bool: 
+        ''' set up wandb run (adds self.run_id), return true if the user wants to continue ''' 
+        
+        if self.debug: return False
+
+        api = wandb.Api()
+        runs = sorted(api.runs(path=f'{self.entity}/{self.project}'), 
+                      key = lambda run: dt.datetime.fromisoformat(run.created_at))
+
+        # If there exists a run on wandb with the same name
+        if not any([self.model_name == run.name for run in runs]): 
+            for run in runs: 
+                if run.name == self.model_name: break 
+
+            # ask the user if they want to continue that run
+            resume = input('Found run \033[1m{run.id}\033[0m on wandb. Continue? [y/n] ')
+            if resume.lower() == 'y': 
+
+                # resume that run and return its id 
+                run = wandb.init(entity = self.entity, project = self.project, 
+                                 id = run.id, resume = 'must')
+                self.run_id = run.id 
+                return True 
+
+            # otherwise, give the user some time to undo if they are wrong
+            time.sleep(3)
+ 
+        # initialise a new wandb run
+        run = wandb.init(entity=self.entity, project=self.project, 
+                         group=self.group, name=self.model_name, 
+                         config=self.__dict__)
+        self.run_id = run.id 
+        return False
+
+    def train(self):
+        ''' train this hyperparam combination, optionally from a checkpoint ''' 
+
+        print(f'\t\033[1m> Training {self.model_name}\033[0m')
+        # Skip if model weights are present 
+        if os.path.exists(self.output_dir) and \
+                os.path.exists(os.path.join(self.output_dir, 'model.safetensors')):
+            print(f'\t{self.model_name} has already been trained; skipping training')
+            return 
+
+        # do the actual training
+        set_all_seeds()
+        trainer = self.trainer
+        trainer.train(resume_from_checkpoint=self.should_continue_wandb())
+        trainer.save_model(self.output_dir)
+
+        # cleanup
+        wandb.finish()
+        del trainer.model
+        del trainer
+
+        # NOTE: this is where you *could* push your model to the hub;
+        # or do that later after you are certain it is solid 
+        # model.push_to_hub(save_dir)
+
+
+    def evaluate(self): 
+        ''' evaluate on babylm, returning score dict and wandb id ''' 
+
+        print(f'\t\033[1m> Evaluating {self.model_name}\033[0m')
+        set_all_seeds()
+        score = eval_and_aggregate(self.output_dir, 0, debug=self.debug)
+
+        # Try to resume the wandb run
+        if self.should_continue_wandb():
+            wandb.log(score)
+            wandb.finish()
+
+        # otherwise just print to console
+        else:
+            print(f'Could not find a run on wandb to save the scores to!!')
+            print(score)
+
+        return score
+
+    def train_and_eval(self, debug=False):
 
         if self.debug: print('\033[1mRUNNING IN DEBUG MODE \033[0m')
 
-        # if output_dir exists, early exit if it contains a model.safetensors file 
-        if not (os.path.exists(self.output_dir) and \
-                os.path.exists(os.path.join(self.output_dir, 'model.safetensors'))):
+        self.train()
+        return self.evaluate()
 
-            set_all_seeds()
-            run_id = None
-            if not self.debug:
-                run = wandb.init(
-                    entity=self.entity, project=self.project, 
-                    group=self.group, name=self.model_name, 
-                    config=self.__dict__)
-                run_id = run.id
 
-            trainer = self.trainer
-            trainer.train()
-            trainer.save_model(self.output_dir)
-
-            # NOTE: this is where you *could* push your model to the hub;
-            # or do that later after you are certain it is solid 
-            # model.push_to_hub(save_dir)
-
-            del trainer.model
-            del trainer
-
-        else: 
-            print(f'\033[1m{self.model_name} has already been trained; skipping training\033[0m')
-
-        set_all_seeds()
-        score = eval_and_aggregate(self.output_dir, 0)
-        print(score)
-
-        # we need to reinitialise as the babylm eval pipeline inits a whole bunch of runs
-        if not self.debug:
-            wandb.init(entity=self.entity, project=self.project, id=run_id, resume='must')
-            wandb.log(score)
-            wandb.finish()
